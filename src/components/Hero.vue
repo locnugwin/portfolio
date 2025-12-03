227785169af16cbef446f696c62de7282921e032
Loc Nguyen <lnguyen72@huskers.unl.edu>
2025-12-02 20:17:27 -0600

    Scaffold Hero component

---
+++ src/components/Hero.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+    import { PhLinkedinLogo, PhGithubLogo } from "@phosphor-icons/vue";
+</script>
+
+<template>
+    <h1>Hi, I'm Loc.</h1>
+    <p>A Computer Science and Mathematics student</p>
+    <PhLinkedinLogo :size="32" weight="fill" />
+    <PhGithubLogo :size="32" weight="fill" />
+</template>
