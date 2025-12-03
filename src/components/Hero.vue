402bfd059d8ecd83b540a8201496642f0a434402
Loc Nguyen <lnguyen72@huskers.unl.edu>
2025-12-02 21:02:00 -0600

    Update Hero component with new description; Add hover color to icons

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -3,13 +3,14 @@
 </script>
 
 <template>
-    <h1>Hi, I'm Loc.</h1>
-    <p>A Computer Science and Mathematics student</p>
+    <h1>LOC NGUYEN</h1>
+    <h1>SOFTWARE ENGINEER</h1>
     <div class="flex items-center gap-2">
         <a 
             href="https://www.linkedin.com/in/locnugwin/" 
             target="_blank" 
             aria-label="Open LinkedIn (opens in new tab)"
+            class="hover:text-neutral-800"
         >
             <PhLinkedinLogo :size="32" />
         </a>
@@ -17,8 +18,10 @@
             href="https://github.com/locnugwin" 
             target="_blank" 
             aria-label="Open GitHub (opens in new tab)"
+            class="hover:text-neutral-800"
         >
             <PhGithubLogo :size="32" />
         </a>
     </div>
+    <p>Striving to create accessible web applications with an interest in writing clean, reusable, and scalable code.</p>
 </template>
