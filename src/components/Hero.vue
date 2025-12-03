a8aeebab7998f04f17de4ec2fcf448b47bc15ee4
Loc Nguyen <lnguyen72@huskers.unl.edu>
2025-12-02 20:33:52 -0600

    Update Hero component to include LinkedIn and GitHub links

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -5,6 +5,20 @@
 <template>
     <h1>Hi, I'm Loc.</h1>
     <p>A Computer Science and Mathematics student</p>
-    <PhLinkedinLogo :size="32" weight="fill" />
-    <PhGithubLogo :size="32" weight="fill" />
+    <div class="flex items-center gap-2">
+        <a 
+            href="https://www.linkedin.com/in/locnugwin/" 
+            target="_blank" 
+            aria-label="Open LinkedIn (opens in new tab)"
+        >
+            <PhLinkedinLogo :size="32" />
+        </a>
+        <a 
+            href="https://github.com/locnugwin" 
+            target="_blank" 
+            aria-label="Open GitHub (opens in new tab)"
+        >
+            <PhGithubLogo :size="32" />
+        </a>
+    </div>
 </template>
