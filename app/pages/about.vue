e8d534834d6dc991954dd403d0621e7ac99e480d
Loc Nguyen <lnguyen72@huskers.unl.edu>
2026-07-22 23:29:27 -0500

    Add initial content and remove about component for experience

--- app/pages/about.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <section>
-    <h1>Hello, World! (from <code>/about</code> page)</h1>
-  </section>
-</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>
-    <h1>Hello, World!</h1>
-  </div>
+  <section>
+    <div>
+      <h1>Loc Nguyen</h1>
+      <p>Software Engineer</p>
+    </div>
+    <div>
+       <button>Email</button>
+       <button>LinkedIn</button>
+       <button>GitHub</button>
+       <button>Resume</button>
+    </div>
+    </section>
+    <section>
+      <p>Hello! I'm Loc, a software engineer.</p>
+      <p>I'm a Master's student studying Computer Science at the University of Nebraska-Lincoln, advised by Prof. Rahul Purandare. Prior, I received my Bachelor's in Computer Science and Mathematics from the University of Nebraska-Lincoln.</p>
+  </section>
 </template>
